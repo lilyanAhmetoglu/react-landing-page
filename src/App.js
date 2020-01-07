8b12b2ab55881f93efbc456d19b5f557867cc874
LilyanAhmet <l.ahmetoglu@qimia.de>
2020-01-07 11:36:35 +0300

    finishing the featured section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './resources/styles.css'
 import Header from './resources/components/header_footer/Header'
+import Featured from './resources/components/featured'
+
 function App() {
   return (
     <div className="App">
        <Header/>
+       <Featured/>
     </div>
   );
 }
