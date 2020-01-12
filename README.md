0a699122d5fe5a8b716fa61dd91dcc4a31874bb6
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-01-12 20:13:48 +0300

    finishing the procet after adding scrolling

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is not responsive, you can only run it on desktop screen.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import './resources/styles.css'
-import Header from './resources/components/header_footer/Header'
-import Featured from './resources/components/featured'
-
+import React from "react";
+import { Element } from "react-scroll";
+import "./resources/styles.css";
+import Header from "./resources/components/header_footer/Header";
+import Featured from "./resources/components/featured";
+import Info from "./resources/components/venueNfo";
+import Highlights from "./resources/components/Highlights";
+import Pricing from "./resources/components/pricing";
+import Location from "./resources/components/location";
+import Footer from "./resources/components/header_footer/Footer";
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <Featured/>
+      <Header />
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="info">
+        <Info />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
+
+      <Footer />
     </div>
   );
 }
